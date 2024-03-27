b4cba32616aa3d2d1aa659d482894cc62383d5a5
vineetbatthina <vineetbatthinaedu@gmail.com>
2024-03-26 20:56:13 -0700

    Navbar init

--- src/App.js
+++ src/App.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import { Switch, Typography } from "@mui/material";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import React, { useState } from "react";
+import DrawerAppBar from "./Components/DrawerAppBar";
 
 const App = () => {
-    return (
-        <div>
-            Vineet Batthina
-        </div>
-    );
-}
+  const [toggleDarkMode, setToggleDarkMode] = useState(false);
+
+  const toggleDarkTheme = () => {
+    setToggleDarkMode(!toggleDarkMode);
+  };
+
+  const darkTheme = createTheme({
+    palette: {
+      mode: toggleDarkMode ? "dark" : "light",
+      primary: {
+        main: toggleDarkMode ? "#000000" : "#FFFFFF",
+      },
+      secondary: {
+        main: toggleDarkMode ? "#000000" : "#000000",
+      },
+    },
+  });
+
+  return (
+    <div>
+      <Switch checked={toggleDarkMode} onChange={toggleDarkTheme} />
+      <Typography color="secondary">Vineet Batthina</Typography>
+      <DrawerAppBar />
+    </div>
+  );
+};
 
 export default App;
